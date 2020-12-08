46b7342df64bb6862b68a0c6e65e6a54aafde9d6
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2020-12-08 15:36:46 -0600

    completes base mode function to console.log new employee object with name, bonus percentage, total compenstation, and total bonus

--- client.js
+++ client.js
@@ -1,34 +1,34 @@
 const employees = [
-  {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
-    reviewRating: 3
-  },
-  {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
-    reviewRating: 4
-  },
-  {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
-    reviewRating: 5
-  },
-  {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
-    reviewRating: 1
-  },
-  {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
-    reviewRating: 1
-  }
+	{
+		name: 'Atticus',
+		employeeNumber: '2405',
+		annualSalary: '47000',
+		reviewRating: 3
+	},
+	{
+		name: 'Jem',
+		employeeNumber: '62347',
+		annualSalary: '63500',
+		reviewRating: 4
+	},
+	{
+		name: 'Scout',
+		employeeNumber: '6243',
+		annualSalary: '74750',
+		reviewRating: 5
+	},
+	{
+		name: 'Robert',
+		employeeNumber: '26835',
+		annualSalary: '66000',
+		reviewRating: 1
+	},
+	{
+		name: 'Mayella',
+		employeeNumber: '89068',
+		annualSalary: '35000',
+		reviewRating: 1
+	}
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -40,39 +40,58 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+console.log(employees);
+
+function createEmployeeObject(employees) {
+
+	for (employee of employees) {
+    let employeeCompensation = {
+      name: employee.name,
+      bonusPercentage: 0,
+      totalCompensation: 0,
+      totalBonus: 0
+    }
+    let bonus = employeeBonusCalculator(employee)
+    employeeCompensation.bonusPercentage = Number(bonus);
+    employeeCompensation.totalCompensation = (Number(bonus) * Number(employee.annualSalary)) + Number(employee.annualSalary);
+    employeeCompensation.totalBonus = Number(bonus) * Number(employee.annualSalary);
+    console.log(employeeCompensation);
+  }
+}
+
+console.log(createEmployeeObject(employees));
 
 
-console.log( employees );
 
-function employeeFunction(employees) {
+
+
+
+
+
+
+function employeeBonusCalculator(employee) {
   let totalBonusPercentageIn = 0;
-  if(employees.employeeNumber.length === 4){
-    totalBonusPercentageIn += .05;
+  if (employee.employeeNumber.length === 4) {
+    totalBonusPercentageIn += 0.05;
   }
   //ratings
-  if (employees.reviewRating === 3) {
-    totalBonusPercentageIn += .04;
+  if (employee.reviewRating === 3) {
+    totalBonusPercentageIn += 0.04;
   }
-  if (employees.reviewRating === 4) {
-    totalBonusPercentageIn += .06;
+  if (employee.reviewRating === 4) {
+    totalBonusPercentageIn += 0.06;
   }
-  if (employees.reviewRating === 5) {
-    totalBonusPercentageIn += .1;
+  if (employee.reviewRating === 5) {
+    totalBonusPercentageIn += 0.1;
   }
-  if(totalBonusPercentageIn > 0.13) {
+  if (totalBonusPercentageIn > 0.13) {
     totalBonusPercentageIn = 0.13;
   }
-  if (employees.annualSalary > 65000) {
-    totalBonusPercentageIn -= .01;
+  if (employee.annualSalary > 65000) {
+    totalBonusPercentageIn -= 0.01;
   }
-  if(totalBonusPercentageIn < 0 ){
+  if (totalBonusPercentageIn < 0) {
     totalBonusPercentageIn = 0;
   }
   return totalBonusPercentageIn.toFixed(2);
 }
-
-console.log(employeeFunction(employees[0]));
-console.log(employeeFunction(employees[1]));
-console.log(employeeFunction(employees[2]));
-console.log(employeeFunction(employees[3]));
-console.log(employeeFunction(employees[4]));
